dc9c16a54da3b437fc2d5131f45801c62ea9d410
lawreka <lawr33ka@gmail.com>
2016-05-24 02:51:01 +0300

    self comic 8 and movers and shakers logo

--- main.js
+++ main.js
@@ -315,7 +315,7 @@ o31 = {
 
 o32 = {
   i: "images/gemlogo.png",
-  t: "Gem logo",
+  t: "Gem logo concept",
   s: "360px x 360px",
   m: "Digital illustration",
   y: "2016",
@@ -1452,7 +1452,25 @@ p120 = {
   d: "p120"
 };
 
-var digiArr = [p74, p63, p57, p90, p84, p85, p88, p87, p86, p97, p100, p98, p99, p102, p103, p104, p95, p94, p91, p78, p77, p79, p50, p51, p81, p75, p62, p56, p58, p92, o34, p55, p46, p45, p83, p93, o28, o27, o30, o29, o31, p49, p54, o25, o32, p41, p61, o9, p108, p109, p110, p113, p117, p118, p119, p120, o7, p47, p116, p114, p96, p89, p80, p76, p67, p65, p66, p82, p70, p69, p68, p72, p73, p71, p53, p48, o36, o26, o33, o22, p115, p111, p112, p101, p107, p105, p106, p60, p59, p52, o24, o23, o19, o18, o20, o17, o15, o14, o13, o12, o16, p64, o35, o21, other, o1];
+p121 = {
+  i: "images/s3lfcomic8.JPG",
+  t: "Self comic 8",
+  s: "2048px x 2732px",
+  m: "Digital illustration",
+  y: "2016",
+  d: "p121"
+};
+
+p122 = {
+  i: "images/moversshakers1.png",
+  t: "Movers & Shakers logo concept",
+  s: "2048px x 2732px",
+  m: "Digital illustration",
+  y: "2016",
+  d: "p122"
+};
+
+var digiArr = [p74, p63, p57, p90, p84, p85, p88, p87, p86, p97, p100, p98, p99, p102, p103, p104, p95, p94, p91, p78, p77, p61, p79, p50, p51, p81, p75, p62, p56, p58, p92, o34, p55, p46, p45, p83, p93, o28, o27, o30, o29, o31, p49, p54, p122, o32, p41, o9, o25, p108, p109, p110, p113, p117, p118, p119, p120, p121, o7, p47, p116, p114, p96, p89, p80, p76, p67, p65, p66, p82, p70, p69, p68, p72, p73, p71, p53, p48, o36, o26, o33, o22, p115, p111, p112, p101, p107, p105, p106, p60, p59, p52, o24, o23, o19, o18, o20, o17, o15, o14, o13, o12, o16, p64, o35, o21, other, o1];
 
 var oilArr = [p15, p23, p9, p7, p21, p2, p3, p14, p25, painting, p12, p19, p6, p5, p4, p1, p11, p13, p16, p8, p20, p10, p18, p24, p17, p22, p26, p28, p29, p30, p27, p34, p31, p32, p33];
 
